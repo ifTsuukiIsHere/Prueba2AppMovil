4082ad1650c39bfe65680400a011edb2b329f129
ChequedMetal <Kostiaandres@gmail.com>
2024-11-19 13:05:45 -0300

    V3.0.0 --solo usar npx cap open android

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBdRtTxwFo05ao0GzybcWCUNHuZ5YzFxvQ",
+  authDomain: "appasistencia-f0092.firebaseapp.com",
+  projectId: "appasistencia-f0092",
+  storageBucket: "appasistencia-f0092.appspot.com",
+  messagingSenderId: "599297931115",
+  appId: "1:599297931115:web:d29b379e3ee96eb8a17e2e",
+  measurementId: "G-9YH5XRQL0F"
+  }
 };
